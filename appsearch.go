b07a1e11dd52c62dd0a8eba5a76e9e22efa05aa0
Balzzanar <spann.johan@gmail.com>
2016-09-24 20:16:52 +0000

    Added more database handling

--- appsearch.go
+++ appsearch.go
@@ -6,7 +6,7 @@ import "regexp"
 //import "reflect"
 import "net/http"
 import "strconv"
-//import "encoding/json"
+import "time"
 
 
 type Config struct {
@@ -21,6 +21,7 @@ type Url struct {
 
 }
 
+var dbh *DBHandler
 
 var useragent string = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0"
 const URL_IMMOBILIEN_SEARCH string = "https://www.immobilienscout24.de/Suche/S-T/Wohnung-Miete/Fahrzeitsuche/M_fcnchen/-/113055/2029726/-/1276002059/60/2,00-/-/EURO--800,00?enteredFrom=one_step_search"
@@ -29,11 +30,26 @@ var GLOBAL_CONFIG Config
 
 func main() {
 	load_config()
+	dbh = new(DBHandler)
+	dbh.Init()
+	defer dbh.Close()
+
 	/* Get all the exposes! */
 	for _,url := range GLOBAL_CONFIG.urls {
-		//get_exposes(url)	
+		get_exposes(url)
 	}
-	
+
+	/* Store all collected exposes */
+	for _,exposeid := range exposelist {
+		expose := &Expose{id: exposeid, url: "https://www.immobilienscout24.de/"+exposeid, first_seen: int(time.Now().Unix())}
+		dbh.StoreExpose(expose)
+	}
+
+	/* Collect information about the exposes */
+	listExpose := dbh.GetAllNonCollectedExposes()
+	for _,expose := range listExpose {
+		collect_expose(&expose)
+	}
 }
 
 func get_exposes(url Url) {
@@ -53,6 +69,26 @@ func get_exposes(url Url) {
 			return
 		}
 	}
+}
+
+/**
+ * Extracts information from the expose, by running regex functions. 
+ */
+func collect_expose(expose *Expose) {
+	fmt.Printf("Running expose collection for id: %s (%s)\n", expose.id, expose.url)
+	result := get_http_data(expose.url)
+	// First check if the expose has been taken down (<h3>Immobilie nicht gefunden.</h3>)
+
+	run_regexp_expose_offline(result, expose)
+	if expose.online == TABLE_EXPOSES_YES {
+		run_regexp_get_cost(result, expose)
+		run_regexp_get_rooms(result, expose)
+		run_regexp_get_size(result, expose)
+		run_regexp_get_pets(result, expose)		
+	}
+
+	expose.collected = TABLE_EXPOSES_YES
+	dbh.UpdateExpose(expose)
 }
 
 
@@ -77,17 +113,78 @@ func load_config() {
 } 
 
 
+
+func run_regexp_get_pets(data []byte, expose *Expose){
+	regexpStr := `<dd class="is24qa-haustiere grid-item three-fifths">\s(.+)\s</dd>`
+	re := regexp.MustCompile(regexpStr)
+	result := re.FindAllStringSubmatch(string(data), -1);
+	expose.pets = TABLE_EXPOSES_YES
+	fmt.Println(result)
+	if len(result) > 0 {
+		if len(result[0]) > 0 {
+			if result[0][1] == "Nein" {
+				expose.pets = TABLE_EXPOSES_NO
+			}
+		}
+	}
+	fmt.Println(expose.pets)
+}
+
+func run_regexp_expose_offline(data []byte, expose *Expose){
+	regexpStr := `<h3>Immobilie nicht gefunden.</h3>`
+	re := regexp.MustCompile(regexpStr)
+	result := re.FindAllStringSubmatch(string(data), -1);
+	expose.online = TABLE_EXPOSES_YES
+	if len(result) > 0 {
+		expose.online = TABLE_EXPOSES_NO
+	}
+}
+
+func run_regexp_get_rooms(data []byte, expose *Expose){
+	regexpStr := `<div class="is24qa-zi is24-value font-semibold">\s(.+)\s</div>`
+	re := regexp.MustCompile(regexpStr)
+	result := re.FindAllStringSubmatch(string(data), -1);
+	rooms,_ := strconv.ParseInt(result[0][1], 10, 64)
+	expose.rooms = int(rooms)
+} 
+
+func run_regexp_get_size(data []byte, expose *Expose){
+	regexpStr := `<div class="is24qa-flaeche is24-value font-semibold">\s(.+)\sm²\s</div>`
+	re := regexp.MustCompile(regexpStr)
+	result := re.FindAllStringSubmatch(string(data), -1);
+	size,_ := strconv.ParseInt(result[0][1], 10, 64)
+	expose.size = int(size)
+} 
+
+/**
+ * Gets the Warm and cold price of the expose.
+ */
+func run_regexp_get_cost(data []byte, expose *Expose){
+	regexpStr := `<div class="is24qa-kaltmiete is24-value font-semibold">\s(.+)\s€\s</div>`
+	re := regexp.MustCompile(regexpStr)
+	result := re.FindAllStringSubmatch(string(data), -1);
+	price_cold,_ := strconv.ParseInt(result[0][1], 10, 64)
+	expose.price_cold = int(price_cold)
+
+	regexpStr = `<dd class="is24qa-nebenkosten grid-item three-fifths"> <span class="is24-operator">.+</span>\s(.+)\s€\s</dd>`
+	re = regexp.MustCompile(regexpStr)
+	result = re.FindAllStringSubmatch(string(data), -1);
+	if len(result) > 0 {
+		if len(result[0]) > 0{
+			price_warm,_ := strconv.ParseInt(result[0][1], 10, 64)
+			expose.price_warm = int(price_warm)			
+		}
+	}
+} 
+
+
 func run_regexp_expose(data []byte) []string{
 	regexpStr := `data-go-to-expose-id="(\d+)"`
 	var exposes []string
-
 	re := regexp.MustCompile(regexpStr)
 	result := re.FindAllStringSubmatch(string(data), -1);
-//	fmt.Printf("%q\n", result)
-//	fmt.Printf("\n%s\n", reflect.TypeOf(result))
 
 	for _,value := range result {
-//		fmt.Printf("%s is of type: %s \n", value, reflect.TypeOf(value))
 		exposes = append(exposes, value[1])
 	}
 	return exposes
--- dbhandler.go
+++ dbhandler.go
@@ -4,14 +4,16 @@ import (
     _ "github.com/mattn/go-sqlite3"
     "database/sql"
     "fmt"
+    "time"
 )
 
 /* -- Constants -- */
 const TABLE_EXPOSES = `create table if not exists exposes 
-						(id int, name text, price_warm int, price_cold int, last_seen int, 
+						(id varchar(20), name text, price_warm int, price_cold int, last_seen int, 
 						first_seen int, care varchar(1), pets varchar(1),
 						zipcode varchar(10), city varchar(30), dist_work int,
-						kausion int);`
+						kausion int, url text, collected varchar(1), rooms int, size int,
+						online varchar(1));`
 
 const DB_FILE_NAME = "./list.db"
 
@@ -19,7 +21,7 @@ const TABLE_EXPOSES_NO = "N"
 const TABLE_EXPOSES_YES = "Y"
 
 type Expose struct {
-	id int
+	id string
 	name string
 	price_warm int
 	price_cold int
@@ -31,6 +33,11 @@ type Expose struct {
 	city string
 	dist_work int
 	kausion int
+	url string
+	collected string
+	rooms int
+	size int
+	online string
 }
 
 
@@ -69,11 +76,12 @@ func (this *DBHandler) Close() {
  * 
  * @name StoreWordlist
  */
-func (this *DBHandler) StoreExposes(expose *Expose) {
-	if ! this.isWordlistUniqe(wordlist.name) {
-		log.Info(fmt.Sprintf("Expose (%d) already exists, updating last seen.", expose.id))
-		// expose.last_seen = ## Current Time ##
-		this.UpdateExpose(expose)
+func (this *DBHandler) StoreExpose(expose *Expose) {
+	if ! this.isExposeUniqe(expose.id) {
+		fmt.Printf("Expose (%s) already exists, updating last seen.\n", expose.id)
+		exposeObj := this.GetExposeById(expose.id)
+		exposeObj.last_seen = int(time.Now().Unix())
+		this.UpdateExpose(&exposeObj)
 		return
 	}
 
@@ -81,12 +89,12 @@ func (this *DBHandler) StoreExposes(expose *Expose) {
 	if err != nil {
 		fmt.Println(err)
 	}
-	stmt, err := tx.Prepare("insert into expose(id, name, price, first_seen, last_seen, care) values(?, ?, ?, ?, ?, ?)")
+	stmt, err := tx.Prepare("insert into exposes(id, name, price_warm, price_cold, first_seen, last_seen, care, pets, zipcode, city, dist_work, kausion, url, collected, rooms, size, online) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
 	if err != nil {
 		fmt.Println(err)
 	}
 	defer stmt.Close()
-	_, err = stmt.Exec(expose.id, expose.name, expose.price, expose.first_seen, expose.last_seen, "")
+	_, err = stmt.Exec(expose.id, expose.name, expose.price_warm, expose.price_cold, expose.first_seen, expose.last_seen, TABLE_EXPOSES_YES, expose.pets, expose.zipcode, expose.city, expose.dist_work, expose.kausion, expose.url, TABLE_EXPOSES_NO, expose.rooms, expose.size, expose.online)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -104,18 +112,55 @@ func (this *DBHandler) UpdateExpose(expose *Expose) {
 	if err != nil {
 		fmt.Println(err)
 	}
-	stmt, err := tx.Prepare("update expose set name=?, price=?, last_seen=?, care=? where id=?")
+	stmt, err := tx.Prepare("update exposes set name=?, price_warm=?, price_cold=?, first_seen=?, last_seen=?, care=?, pets=?, zipcode=?, city=?, dist_work=?, kausion=?, url=?, collected=?, rooms=?, size=?, online=? where id=?")
 	if err != nil {
 		fmt.Println(err)
 	}
 	defer stmt.Close()
-	_, err = stmt.Exec(expose.name, expose.price, expose.last_seen, expose.care, expose.id)
+	_, err = stmt.Exec(expose.name, expose.price_warm, expose.price_cold, expose.first_seen, expose.last_seen, expose.care, expose.pets, expose.zipcode, expose.city, expose.dist_work, expose.kausion, expose.url, expose.collected, expose.rooms, expose.size, expose.online, expose.id)
 	if err != nil {
 		fmt.Println(err)
 	}
 	tx.Commit()
 }
 
+
+func (this *DBHandler) GetAllNonCollectedExposes() []Expose {
+	listexpose := []Expose{}
+	rows, err := this.db.Query("select * from exposes where collected = '"+TABLE_EXPOSES_NO+"'")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var expose Expose
+		err = rows.Scan(&expose.id, &expose.name, &expose.price_warm, &expose.price_cold, &expose.first_seen, &expose.last_seen, &expose.care, &expose.pets, &expose.zipcode, &expose.city, &expose.dist_work, &expose.kausion, &expose.url, &expose.collected, &expose.rooms, &expose.size, &expose.online)
+		if err != nil {
+			fmt.Println(err)
+		}
+		listexpose = append(listexpose, expose)
+	}
+	err = rows.Err()
+	if err != nil {
+		fmt.Println(err)
+	}
+	return listexpose
+}
+
+
+func (this *DBHandler) GetExposeById(id string) Expose {
+	stmt, err := this.db.Prepare("select * from exposes where id = ?")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer stmt.Close()
+	var expose Expose
+	err = stmt.QueryRow(id).Scan(&expose.id, &expose.name, &expose.price_warm, &expose.price_cold, &expose.first_seen, &expose.last_seen, &expose.care, &expose.pets, &expose.zipcode, &expose.city, &expose.dist_work, &expose.kausion, &expose.url, &expose.collected, &expose.rooms, &expose.size, &expose.online)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return expose
+}
 
 
 /**
@@ -126,7 +171,7 @@ func (this *DBHandler) UpdateExpose(expose *Expose) {
 func (this *DBHandler) createNewTable(tablescript string) {
 	_, err := this.db.Exec(tablescript)
 	if err != nil {
-		log.Error(fmt.Sprintf("%q: %s\n", err, tablescript))
+		fmt.Printf("%q: %s\n", err, tablescript)
 		return
 	}
 }
@@ -139,7 +184,7 @@ func (this *DBHandler) createNewTable(tablescript string) {
  * @name isExposeUniqe
  * @return bool
  */
-func (this *DBHandler) isExposeUniqe(id int) bool {
+func (this *DBHandler) isExposeUniqe(id string) bool {
 	stmt, err := this.db.Prepare("select count(*) from exposes where id = ?")
 	if err != nil {
 		fmt.Println(err)
